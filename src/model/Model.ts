b7ec82c6dbecd71e916d78a090ed7cf26e04755f
KeyzarRasya <keyzar.r.athallah@gmail.com>
2025-11-25 23:29:42 +0700

    res.model odoo class

---
+++ src/model/Model.ts
@@ -0,0 +1,152 @@
+import { error } from "console";
+import type { Odoo, OdooConfig } from "./Odoo";
+
+interface Executable{
+    id: number;
+    modelName: string;
+    baseUrl: string;
+    apiKey: string;
+}
+
+type SearchDomain = [string, string, any]
+
+class ModelExecutable {
+    private executable: Executable;
+    constructor(executable: Executable) {
+        this.executable = executable;
+    }
+
+    private getHeaders(includeAuth: boolean): Record<string, string> {
+        let headers: Record<string, string> = {
+            'Content-Type': 'application/json'
+        };
+
+        if (includeAuth) {
+            if (this.executable.apiKey) {
+                headers['Authorization'] = `Bearer ${this.executable.apiKey}`
+            }
+        }
+
+        return headers;
+    }
+
+    private async handleResponse(response: Response): Promise<void> {
+        if (!response.ok) {
+            let err: any;
+
+            try {
+                err = await response.json();
+            } catch {
+                err = { message: "Network Error" };
+            }
+
+            throw new Error(
+                err?.message || err?.error || JSON.stringify(err) || "Unknown error"
+            );
+        }
+        return response.json()
+    }
+
+    private async request(endpoint: string, options: any): Promise<void> {
+        try {
+            let url: string = `${this.executable.baseUrl}${endpoint}`;
+            const response: Response = await fetch(url, {
+                ...options,
+                headers: this.getHeaders(options.includeAuth !== false)
+            })
+            return await this.handleResponse(response);
+        } catch(error) {
+            console.error(`API ERROR: ${endpoint}`, error);
+            throw error;
+        }
+    }
+
+    async write(data: any): Promise<void> {
+        return this.request(`/json/2/${this.executable.modelName}/write`, {
+            method: "POST",
+            body: JSON.stringify({
+                ids: [this.executable.id],
+                vals: data
+            })
+        })
+    }
+}
+
+export class Model {
+    private odooConfig: OdooConfig;
+    private modelName: string;
+
+    constructor(odooConfiguration: OdooConfig, modelName: string) {
+        this.odooConfig = odooConfiguration;
+        this.modelName = modelName;
+    }
+
+    private getHeaders(includeAuth: boolean): Record<string, string> {
+        let headers: Record<string, string> = {
+            'Content-Type': 'application/json'
+        };
+
+        if (includeAuth) {
+            if (this.odooConfig.apiKey) {
+                headers['Authorization'] = `Bearer ${this.odooConfig.apiKey}`
+            }
+        }
+
+        return headers;
+    }
+
+    private async handleResponse(response: Response): Promise<any> {
+        if (!response.ok) {
+            let err: any;
+
+            try {
+                err = await response.json();
+            } catch {
+                err = { message: "Network Error" };
+            }
+
+            throw new Error(
+                err?.message || err?.error || JSON.stringify(err) || "Unknown error"
+            );
+        }
+        return response.json()
+    }
+
+    private async request(endpoint: string, options: any): Promise<any> {
+        try {
+            let url: string = `${this.odooConfig.baseUrl}${endpoint}`;
+            const response: Response = await fetch(url, {
+                ...options,
+                headers: this.getHeaders(options.includeAuth !== false)
+            })
+            return await this.handleResponse(response);
+        } catch(error) {
+            console.error(`API ERROR: ${endpoint}`, error);
+            throw error;
+        }
+    }
+
+    id(id: number): ModelExecutable {
+        return new ModelExecutable({
+            id: id,
+            modelName: this.modelName,
+            baseUrl: this.odooConfig.baseUrl,
+            apiKey: this.odooConfig.apiKey
+        });
+    }
+
+    async create(data: any): Promise<any> {
+        return this.request(`/json/2/${this.modelName}/create`, {
+            method: "POST",
+            body: JSON.stringify({vals_list: data})
+        });
+    }
+
+    async search_read(domains: SearchDomain[], fields: string[]): Promise<any> {
+        return this.request(`/json/2/${this.modelName}/search_read`, {
+            method: "POST",
+            body: JSON.stringify({domain: domains, fields: fields})
+        })
+    }
+
+}
